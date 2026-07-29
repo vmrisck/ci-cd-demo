d2b9fc112a8f0a0dc22b3bf431cfe2ce811c1515
vmrisck@gmail.com <vmrisck@gmail.com>
2026-07-29 14:34:23 +0530

    Revert "simulate failing test"
    This reverts commit 4cdece0cbde465320e3c6a66c474f70704f58368.

--- test/index.test.js
+++ test/index.test.js
@@ -3,5 +3,5 @@ const assert = require('node:assert');
 const { add } = require('../index.js');
 
 test('add adds two numbers', () => {
-  assert.strictEqual(add(2, 3), 6);
+  assert.strictEqual(add(2, 3), 5);
 });
