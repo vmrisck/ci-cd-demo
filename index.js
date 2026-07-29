e9133a653333f849c4933215811c9402b9763fb6
vmrisck@gmail.com <vmrisck@gmail.com>
2026-07-29 14:18:41 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function add(a, b) {
+  return a + b;
+}
+
+console.log(add(2, 3));
+
+module.exports = { add };
---
+++ test/index.test.js
@@ -0,0 +1,7 @@
+const test = require('node:test');
+const assert = require('node:assert');
+const { add } = require('../index.js');
+
+test('add adds two numbers', () => {
+  assert.strictEqual(add(2, 3), 5);
+});
